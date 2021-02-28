209856e1fd222ebc3003b7c5e4c096d239f81b66
Serg Kotovsky <rokulab@mail.ru>
2021-02-28 19:26:55 +0300

    client

---
+++ client/go.mod
@@ -0,0 +1,8 @@
+module github.com/Segga/client
+
+go 1.16
+
+require (
+	github.com/Seggga/testmodule v1.0.1
+	github.com/valyala/fasthttp v1.21.0 // indirect
+)
---
+++ client/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"github.com/Seggga/testmodule"
+	//testmodule "github.com/Seggga/testmodule/v2"
+)
+
+func main() {
+
+	println(testmodule.ShowDep())
+}
